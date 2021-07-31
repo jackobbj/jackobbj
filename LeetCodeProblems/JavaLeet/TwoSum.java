7b744a9ca6a332c1f68cc5416f1e93cc5cc745a3
jackobbj <jack.hodges1999@live.com>
2021-07-31 16:53:23 +1000

    initial commit and added leetcode problems file and java TwoSum solution

---
+++ LeetCodeProblems/JavaLeet/TwoSum.java
@@ -0,0 +1,21 @@
+package LeetCodeProblems.JavaLeet;
+import java.util.HashMap;
+public class TwoSum {
+    /**A Class designed to Solve the Two Sum problem defined as:
+     * 
+     * "Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+     * You may assume that each input would have exactly one solution, and you may not use the same element twice.
+     * You can return the answer in any order."
+     */
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer,Integer> tracker = new HashMap<Integer,Integer>(); // stores element pair to achieve target
+        for (int i=0; i <nums.length; i++){
+            if (tracker.containsKey(nums[i])){
+                int[] x = {i,tracker.get(nums[i]).intValue()};
+                return x;
+            };
+            tracker.put(target-nums[i], i); 
+        };
+        return null; // needed to compile but not needed to work
+    }
+  };
